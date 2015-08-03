341309cbefe1b7a74e3a179678f83458d7c90c8d
ehawk823 <evan.alexander.hawk@gmail.com>
2015-08-02 20:00:47 -0500

    done

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  if array.length > 2
+    last_item = array.pop
+    array << "and #{last_item}"
+    updated = array.join(", ")
+  end
+  if array.length == 2
+    updated = array.join(" and ")
+  end
+  if array.length < 2
+    updated = array.join
+  end
+  updated
 end
